ed583d9c298c826119666ca5e1d0396b4956f72c
Abhineet-08 <113853385+Abhineet-08@users.noreply.github.com>
2023-03-01 13:38:47 +0530

    Delete contact.js

--- contact.js
+++
@@ -1,6 +0,0 @@
-console.log('Contact page loaded.');
-
-document.querySelector('form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  console.log('Form submitted!');
-});
